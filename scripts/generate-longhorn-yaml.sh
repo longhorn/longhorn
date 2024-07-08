09b1a8ebc79a1c083090bbfd96c64667d77964e0
James Lu <james.lu@suse.com>
2024-07-08 22:34:43 +0800

    fix(deploy): add a deploying manifest file with Openshift enabled.
    ref: longhorn/longhorn 8300
    
    Signed-off-by: James Lu <james.lu@suse.com>

--- scripts/generate-longhorn-yaml.sh
+++ scripts/generate-longhorn-yaml.sh
@@ -5,7 +5,7 @@ set -o xtrace
 
 PRJ_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/.." 2>/dev/null || realpath "$(dirname "${BASH_SOURCE[0]}")/.." 2>/dev/null)
 CHART_DIR="$PRJ_DIR/chart"
-DEPLOY_YAML="$PRJ_DIR/deploy/longhorn.yaml"
+DEPLOY_YAMLS=("$PRJ_DIR/deploy/longhorn.yaml" "$PRJ_DIR/deploy/longhorn-okd.yaml")
 DEPLOY_YAML_TMP="$PRJ_DIR/deploy/longhorn.yaml.tmp"
 NAMESPACE=${NAMESPACE:-longhorn-system}
 
@@ -14,7 +14,8 @@ if ! command -v helm &> /dev/null || ! helm version --short | grep -q "v3"; then
   exit 1
 fi
 
-cat <<EOD > "$DEPLOY_YAML"
+for DEPLOY_YAML in ${DEPLOY_YAMLS[@]}; do
+  cat <<EOD > "$DEPLOY_YAML"
 ---
 # Builtin: "helm template" does not respect --create-namespace
 apiVersion: v1
@@ -23,8 +24,13 @@ metadata:
   name: $NAMESPACE
 EOD
 
-helm template longhorn "$CHART_DIR" --namespace "$NAMESPACE" --create-namespace --no-hooks >>"$DEPLOY_YAML"
-< "$DEPLOY_YAML" grep -v 'helm.sh\|app.kubernetes.io/managed-by: Helm' | grep -v "helm.sh/chart:" > "$DEPLOY_YAML_TMP"
-mv "$DEPLOY_YAML_TMP" "$DEPLOY_YAML"
+  if [[ $DEPLOY_YAML == $PRJ_DIR/deploy/longhorn-okd.yaml ]]; then
+    OKD_ENABLED_FLAG="--set openshift.enabled=true"
+  fi
 
+  helm template longhorn "$CHART_DIR" --namespace "$NAMESPACE" $OKD_ENABLED_FLAG --create-namespace --no-hooks >>"$DEPLOY_YAML"
+  < "$DEPLOY_YAML" grep -v 'helm.sh\|app.kubernetes.io/managed-by: Helm' | grep -v "helm.sh/chart:" > "$DEPLOY_YAML_TMP"
+  mv "$DEPLOY_YAML_TMP" "$DEPLOY_YAML"
+
+done
 
