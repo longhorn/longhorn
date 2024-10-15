97b61834a81802e4b52adf84b91a613d1150cf26
Derek Su <derek.su@suse.com>
2024-10-15 11:51:14 +0800

    fix(workflow): fix script paths
    Signed-off-by: Derek Su <derek.su@suse.com>

--- .github/workflows/scan-and-notify-testing-items.py
+++ .github/workflows/scan-and-notify-testing-items.py
@@ -303,6 +303,6 @@ def scan_and_notify(github_org, github_repo, github_project):
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
-        print('Usage: python scan_and_notify_testing_items.py <github_org> <github_repo> <github_project>')
+        print('Usage: python scan-and-notify-testing-items.py <github_org> <github_repo> <github_project>')
 
     scan_and_notify(sys.argv[1], sys.argv[2], sys.argv[3])
