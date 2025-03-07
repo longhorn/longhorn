90a629d01bb9c5796e1b5bef74581284a1707ebd
Chin-Ya Huang <chin-ya.huang@suse.com>
2025-03-07 12:11:16 +0800

    feat(upgrade_responder): add longhornBackingImageCount to requestSchema
    longhorn/longhorn-7599
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- dev/upgrade-responder/install.sh
+++ dev/upgrade-responder/install.sh
@@ -242,6 +242,9 @@ configMap:
         }
       },
       "extraFieldInfoSchema": {
+        "longhornBackingImageCount": {
+          "dataType": "float"
+        },
         "longhornDiskBlockCount": {
           "dataType": "float"
         },
